ba8849a747fb62cff1375050b656e3cfa723d269
MaxD <max.moderation@gmail.com>
2022-05-20 10:07:48 +0300

    feat: end task, 'bug: not working bye-msg with Ctrl+C in console' (20 май 2022 г. 10:07:48)

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout, stdin, exit } = require('process');
+
+let output = fs.createWriteStream(path.join(__dirname, 'enteredText.txt'), {
+  flags: 'a',
+});
+
+stdout.write('Enter any text....\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    exit();
+  }
+  output.write(data);
+});
+
+process.on('exit', () => {
+  stdout.write('Good bye!');
+});
