a45fb21ed3b15c3878d87d8605c8268ab5a14145
MaxD <max.moderation@gmail.com>
2022-05-22 14:20:53 +0300

    feat: task copy-files done (22 май 2022 г. 14:20:53)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const path = require('path');
+const fs = require('fs');
 
+const dirPath = path.join(__dirname, 'files');
+const dirPathCopy = path.join(__dirname, 'files-copy');
+
+(function initDir() {
+  fs.rm(dirPathCopy, { recursive: true }, (err) => {
+    if (err) {
+      copyDir(dirPath, dirPathCopy);
+    } else {
+      copyDir(dirPath, dirPathCopy);
+    }
+  });
+})();
+
+function copyDir(from, destination) {
+  fs.mkdir(destination, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(from, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+
+    for (item of files) {
+      if (item.isFile()) {
+        fs.copyFile(path.join(from, item.name), path.join(destination, item.name), fs.constants.COPYFILE_FICLONE, (err) => {
+          if (err) throw err;
+        });
+      } else {
+        copyDir(path.join(dirPath, item.name), path.join(dirPathCopy, `${item.name}`));
+      }
+    }
+  });
+}
