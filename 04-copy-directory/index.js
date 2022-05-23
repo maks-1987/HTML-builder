42a09d8b1960c6b19fcfd80d0ed1986432d1ef98
MaxD <max.moderation@gmail.com>
2022-05-23 18:09:26 +0300

    refactor: change to modules

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -14,22 +14,24 @@ const dirPathCopy = path.join(__dirname, 'files-copy');
   });
 })();
 
-function copyDir(from, destination) {
+async function copyDir(from, destination) {
   fs.mkdir(destination, { recursive: true }, (err) => {
     if (err) throw err;
-  });
 
-  fs.readdir(from, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
+    fs.readdir(from, { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
 
-    for (item of files) {
-      if (item.isFile()) {
-        fs.copyFile(path.join(from, item.name), path.join(destination, item.name), fs.constants.COPYFILE_FICLONE, (err) => {
-          if (err) throw err;
-        });
-      } else {
-        copyDir(path.join(dirPath, item.name), path.join(dirPathCopy, `${item.name}`));
+      for (item of files) {
+        if (item.isFile()) {
+          fs.copyFile(path.join(from, item.name), path.join(destination, item.name), (err) => {
+            if (err) throw err;
+          });
+        } else {
+          copyDir(path.join(from, item.name), path.join(destination, item.name));
+        }
       }
-    }
+    });
   });
 }
+
+module.exports = { copyDir };
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const dirStyles = path.join(__dirname, 'styles');
 const dirProject = path.join(__dirname, 'project-dist');
 
-function mergeData() {
-  fs.readdir(dirStyles, { withFileTypes: true }, (err, files) => {
+function mergeData(from, to, cssName) {
+  fs.readdir(from, { withFileTypes: true }, (err, files) => {
     if (err) throw err;
-    let writeStream = fs.createWriteStream(path.join(dirProject, 'bundle.css'), { flags: 'a' });
+    let writeStream = fs.createWriteStream(path.join(to, cssName), { flags: 'a' });
 
     for (item of files) {
       if (item.isFile() && path.extname(item.name) === '.css') {
-        fs.readFile(path.join(dirStyles, item.name), (err, data) => {
+        fs.readFile(path.join(from, item.name), (err, data) => {
           if (err) throw err;
           writeStream.write(data.toString() + '\n');
         });
@@ -20,4 +20,6 @@ function mergeData() {
   });
 }
 
-mergeData();
+mergeData(dirStyles, dirProject, 'bundle.css');
+
+module.exports = { mergeData };
